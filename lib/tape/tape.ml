8684610a62a07f9c9c083aede332a291c0be7556
difof <hesyoamaster@protonmail.com>
2025-08-17 14:12:00 +0330

    feat: added tape signature

---
+++ lib/tape/tape.ml
@@ -0,0 +1,12 @@
+module type S = sig
+  type t
+
+  val create : unit -> t
+  val get : t -> int
+  val set : t -> int -> unit
+  val pos : t -> int
+  val move : t -> int -> unit
+end
+
+module HashTape = Hashtape
+module ArrayTape = Arraytape
