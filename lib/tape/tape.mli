df8da49c35962c3349751c9f142795f289d201df
difof <hesyoamaster@protonmail.com>
2025-08-20 15:14:00 +0330

    refactor: added bytes tape interface

--- lib/tape/tape.mli
+++ lib/tape/tape.mli
@@ -1,12 +1,69 @@
-module type S = sig
-  type t
+(* Bi-directional size capped linear tape memory of bytes *)
 
-  val create : unit -> t
-  val get : t -> int
-  val set : t -> int -> unit
-  val pos : t -> int
-  val move : t -> int -> unit
-end
+type t
 
-module Hashtape : S
-module Arraytape : S
+type bias_offset =
+  | Start
+  | Middle
+
+type error =
+  | MaxAllocationReached of int * int
+  | BlitOutOfBounds of int * int * int
+  | BlitNegativeLength of int
+
+exception TapeExn of error
+
+(* Prints the Tape exceptions *)
+val pp_error : Format.formatter -> error -> unit
+
+(* Create a new Tape. The initial size will have minimum size of 256 *)
+val create : ?bias_offset:bias_offset -> ?max_size:int -> int -> t
+
+(*
+   Move the Tape pointer by given offset. It can be negative.
+    Throws `MaxAllocationReached`
+*)
+val move_exn : t -> int -> unit
+val move : t -> int -> (unit, error) result
+
+(* Get the value of current Tape pointer *)
+val get : t -> int
+
+(* Set the value of current Tape pointer masked by 0xFF *)
+val set : t -> int -> unit
+
+(* Get the total length of the Tape *)
+val len : t -> int
+
+(* Get the center bias of the Tape, used for negative/positive indexing *)
+val bias : t -> int
+
+(* Get the logical Tape pointer position, it can be negative *)
+val logical_pos : t -> int
+
+(* Get the computed physical Tape pointer position in the buffer *)
+val physical_pos : t -> int
+
+(* Get the max size of the Tape. It cannot grow larger than this *)
+val max_size : t -> int
+
+(*
+   Get a slice of the Tape. 
+    If given size is larger than Tape's current position + len, it will throw `BlitOutOfBounds`
+*)
+val blit_out_exn : t -> bytes -> int -> unit
+val blit_out : t -> bytes -> int -> (unit, error) result
+
+(*
+   Copy the given buffer's contents into Tape, starting at current tape pointer. 
+    If Tape limits are reached, it won't grow and throw `BlitOutOfBounds` instead.
+*)
+val blit_in_exn : t -> bytes -> int -> unit
+val blit_in : t -> bytes -> int -> (unit, error) result
+
+(*
+   Copy the given buffer's contents into Tape, starting at current tape pointer. 
+    It will grow the Tape as needed.
+*)
+val blit_in_ensure_exn : t -> bytes -> int -> unit
+val blit_in_ensure : t -> bytes -> int -> (unit, error) result
