c758f6e4bd3c279b8dd89422ee90067ae852528e
difof <hesyoamaster@protonmail.com>
2025-08-25 14:58:55 +0330

    refactor: updated compiler interface

--- lib/encoder/compiler.mli
+++ lib/encoder/compiler.mli
@@ -3,10 +3,15 @@ type intermediate_instr =
   | OpenLoop
   | CloseLoop
 
+type error =
+  | UnmatchedClosingBracket
+  | UnmatchedOpeningBracket
+  | EncodingError of Instruction.error
+
 val parse_sequence : string -> intermediate_instr list
 val optimize_instructions : intermediate_instr list -> intermediate_instr list
 val pattern_optimize : intermediate_instr list -> intermediate_instr list
-val resolve_jumps : intermediate_instr list -> Instruction.t list
-val encode_to_bytes : Instruction.t list -> bytes list
+val resolve_jumps : intermediate_instr list -> (Instruction.t list, error) result
+val encode_to_bytes : Instruction.t list -> (bytes list, error) result
 val combine_instruction_bytes : bytes list -> bytes
-val compile : string -> bytes
+val compile : string -> (bytes, error) result
