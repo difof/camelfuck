3ddbc319654b3ba118077c6dc01d68671c0d2149
difof <hesyoamaster@protonmail.com>
2025-08-28 14:49:20 +0330

    refactor: added vm interface

---
+++ lib/runtime/vm.mli
@@ -0,0 +1,14 @@
+type t
+
+type error =
+  | BytecodeOutOfBounds of int
+  | JumpOutOfBounds of int
+  | TapeError of Tape.error
+  | Exception of exn
+
+val pp_error : Format.formatter -> error -> unit
+
+exception VMExn of error
+
+val create : ?io:in_channel * out_channel -> ?memory:Tape.t -> bytes -> t
+val run : t -> (t, error) result
