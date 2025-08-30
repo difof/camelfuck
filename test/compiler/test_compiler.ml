488ee72129d77a0c7e5cd1174eae7f6fb7cc164c
difof <hesyoamaster@protonmail.com>
2025-08-30 14:41:40 +0330

    test: added tests for new compiler and isa

---
+++ test/compiler/test_compiler.ml
@@ -0,0 +1,192 @@
+open Core
+open Compiler
+open Isa
+
+let show_intermediate = function
+  | Instr i -> Format.asprintf "Instr(%a)" pp_t i
+  | OpenLoop -> "OpenLoop"
+  | CloseLoop -> "CloseLoop"
+;;
+
+let expect_parse name source expected =
+  let actual = parse_sequence source in
+  let expected_s = List.map ~f:show_intermediate expected in
+  let actual_s = List.map ~f:show_intermediate actual in
+  Alcotest.(check (list string)) name expected_s actual_s
+;;
+
+let test_basic () =
+  expect_parse
+    "basic tokens"
+    "+-<>.,[]"
+    [ Instr (AddN 1)
+    ; Instr (AddN (-1))
+    ; Instr (MoveN (-1))
+    ; Instr (MoveN 1)
+    ; Instr Out
+    ; Instr In
+    ; OpenLoop
+    ; CloseLoop
+    ]
+;;
+
+let test_counts_and_caps () =
+  let make c n = String.make n c in
+  expect_parse "plus fused" "++++" [ Instr (AddN 4) ];
+  expect_parse "minus fused" "----" [ Instr (AddN (-4)) ];
+  expect_parse "right fused" ">>>>" [ Instr (MoveN 4) ];
+  expect_parse "left fused" "<<<<" [ Instr (MoveN (-4)) ];
+  (* capping *)
+  let s = make '+' 300 in
+  expect_parse "plus capped" s [ Instr (AddN 127); Instr (AddN 127); Instr (AddN 46) ];
+  let s = make '-' 300 in
+  expect_parse
+    "minus capped"
+    s
+    [ Instr (AddN (-128)); Instr (AddN (-128)); Instr (AddN (-44)) ];
+  let s = make '>' 300 in
+  expect_parse "right capped" s [ Instr (MoveN 127); Instr (MoveN 127); Instr (MoveN 46) ];
+  let s = make '<' 300 in
+  expect_parse
+    "left capped"
+    s
+    [ Instr (MoveN (-128)); Instr (MoveN (-128)); Instr (MoveN (-44)) ]
+;;
+
+let test_loops () =
+  expect_parse "empty loop" "[]" [ OpenLoop; CloseLoop ];
+  expect_parse "nested loops" "[[]]" [ OpenLoop; OpenLoop; CloseLoop; CloseLoop ]
+;;
+
+let test_ignore_noise () =
+  expect_parse "ignores spaces and others" "a b\n\t+" [ Instr (AddN 1) ]
+;;
+
+let show_instr t = Format.asprintf "%a" pp_t t
+
+let show_error = function
+  | UnmatchedClosingBracket _ -> "UnmatchedClosingBracket"
+  | UnmatchedOpeningBracket _ -> "UnmatchedOpeningBracket"
+;;
+
+let expect_resolve name source expected =
+  match parse_sequence source |> resolve_jumps with
+  | Ok actual ->
+    Alcotest.(check (list string))
+      name
+      (List.map ~f:show_instr expected)
+      (List.map ~f:show_instr actual)
+  | Error err -> Alcotest.failf "%s: expected Ok, got Error %a" name Compiler.pp_error err
+;;
+
+let expect_resolve_error name source expected_err =
+  match parse_sequence source |> resolve_jumps with
+  | Ok _ -> Alcotest.failf "%s: expected Error, got Ok" name
+  | Error err -> Alcotest.(check string) name expected_err (show_error err)
+;;
+
+let expect_resolve_exact name source expected =
+  match parse_sequence source |> resolve_jumps with
+  | Ok actual ->
+    Alcotest.(check (list string))
+      name
+      (List.map ~f:show_instr expected)
+      (List.map ~f:show_instr actual)
+  | Error err -> Alcotest.failf "%s: expected Ok, got Error %a" name Compiler.pp_error err
+;;
+
+let test_resolve_empty_loop () =
+  (* intermediates: [ Open, Close ]
+     Jz jumps to after Close; Jnz jumps back to Open *)
+  expect_resolve "empty loop" "[]" [ Jz 2; Jnz 0 ]
+;;
+
+let test_resolve_with_body () =
+  (* pattern: [+ -] inside a loop. We only assert relative correctness shape, not exact bytes *)
+  match parse_sequence "[+-]" |> resolve_jumps with
+  | Error err -> Alcotest.failf "resolve error: %a" Compiler.pp_error err
+  | Ok instrs ->
+    (* should be: Jz k; AddN 1; AddN -1; Jnz m *)
+    (match instrs with
+     | [ Jz _; AddN 1; AddN -1; Jnz _ ] -> ()
+     | _ -> Alcotest.fail "unexpected resolved sequence")
+;;
+
+let test_resolve_with_body_exact () =
+  expect_resolve_exact "with body exact" "[+-]" [ Jz 4; AddN 1; AddN (-1); Jnz (-2) ]
+;;
+
+let test_resolve_nested () =
+  match parse_sequence "[[]]" |> resolve_jumps with
+  | Error err -> Alcotest.failf "resolve error: %a" Compiler.pp_error err
+  | Ok instrs ->
+    (* Expect: outer Jz _, inner Jz _, inner Jnz _, outer Jnz _ *)
+    (match instrs with
+     | [ Jz _; Jz _; Jnz _; Jnz _ ] -> ()
+     | _ -> Alcotest.fail "unexpected nested jumps resolution")
+;;
+
+let test_resolve_nested_exact () =
+  expect_resolve_exact "nested exact" "[[]]" [ Jz 4; Jz 2; Jnz 0; Jnz (-2) ]
+;;
+
+let test_resolve_mixed_exact () =
+  expect_resolve_exact
+    "mixed exact"
+    "+[>+<-]-"
+    [ AddN 1; Jz 6; MoveN 1; AddN 1; MoveN (-1); AddN (-1); Jnz (-4); AddN (-1) ]
+;;
+
+let test_resolve_errors () =
+  expect_resolve_error "unmatched close" "]" "UnmatchedClosingBracket";
+  expect_resolve_error "unmatched open" "[" "UnmatchedOpeningBracket"
+;;
+
+let test_fuse_std_ops () =
+  let instrs = parse_sequence "+++-->>><<<" |> fuse_std_ops in
+  let s = List.map ~f:show_intermediate instrs in
+  Alcotest.(check (list string)) "fused" [ "Instr(addn(1))" ] s
+;;
+
+let test_optimize_patterns () =
+  let expect name source expected =
+    let actual = parse_sequence source |> fuse_std_ops |> optimize_patterns in
+    let s_expected = List.map ~f:show_intermediate expected in
+    let s_actual = List.map ~f:show_intermediate actual in
+    Alcotest.(check (list string)) name s_expected s_actual
+  in
+  expect "clear" "[-]" [ Instr Clear ];
+  expect "scan right" "[>]" [ Instr (ScanN 1) ];
+  expect "scan left" "[<]" [ Instr (ScanN (-1)) ];
+  expect "transfer right" "[>+<-]" [ Instr (TransferN 1) ];
+  expect "transfer left" "[<+>-]" [ Instr (TransferN (-1)) ];
+  expect "addat" ">>+<<" [ Instr (AddAt (2, 1)) ];
+  expect "multransfer r" "[->+>>+>++<<<<]" [ Instr (MulTransfer [ 1, 1; 3, 1; 4, 2 ]) ];
+  expect "multransfer l" "[<+<<+<++>>>>-]" [ Instr (MulTransfer [ -4, 2; -3, 1; -1, 1 ]) ]
+;;
+
+let () =
+  let open Alcotest in
+  run
+    "Compiler (refactored) tests"
+    [ ( "parse_sequence"
+      , [ test_case "basic" `Quick test_basic
+        ; test_case "counts and caps" `Quick test_counts_and_caps
+        ; test_case "loops" `Quick test_loops
+        ; test_case "ignore" `Quick test_ignore_noise
+        ] )
+    ; ( "resolve_jumps"
+      , [ test_case "empty" `Quick test_resolve_empty_loop
+        ; test_case "with body" `Quick test_resolve_with_body
+        ; test_case "nested" `Quick test_resolve_nested
+        ; test_case "errors" `Quick test_resolve_errors
+        ; test_case "with body exact" `Quick test_resolve_with_body_exact
+        ; test_case "nested exact" `Quick test_resolve_nested_exact
+        ; test_case "mixed exact" `Quick test_resolve_mixed_exact
+        ] )
+    ; ( "optimize"
+      , [ test_case "fuse_std_ops" `Quick test_fuse_std_ops
+        ; test_case "patterns" `Quick test_optimize_patterns
+        ] )
+    ]
+;;
---
+++ test/isa/test_isa.ml
@@ -0,0 +1,131 @@
+open Isa
+
+let bytes_to_list b =
+  let len = Bytes.length b in
+  let rec loop acc i =
+    if i = len then List.rev acc else loop (Bytes.get_uint8 b i :: acc) (i + 1)
+  in
+  loop [] 0
+;;
+
+let expect_ok_bytes name instr expected_bytes =
+  match encode instr with
+  | Ok b -> Alcotest.(check (list int)) name expected_bytes (bytes_to_list b)
+  | Error _ -> Alcotest.fail (name ^ ": expected Ok, got Error")
+;;
+
+let expect_error name instr =
+  match encode instr with
+  | Ok _ -> Alcotest.fail (name ^ ": expected Error, got Ok")
+  | Error _ -> ()
+;;
+
+let int32_bytes_le i =
+  let b = Bytes.create 4 in
+  Bytes.set_int32_le b 0 (Int32.of_int i);
+  [ Bytes.get_uint8 b 0; Bytes.get_uint8 b 1; Bytes.get_uint8 b 2; Bytes.get_uint8 b 3 ]
+;;
+
+let test_to_char_and_size () =
+  (* opcode mapping and byte_size checks *)
+  Alcotest.(check char) "Hang opcode" (Char.chr 0x00) (to_char Hang);
+  Alcotest.(check int) "Hang size" 1 (byte_size Hang);
+  Alcotest.(check char) "AddN opcode" (Char.chr 0x01) (to_char (AddN 0));
+  Alcotest.(check int) "AddN size" 2 (byte_size (AddN 0));
+  Alcotest.(check char) "MoveN opcode" (Char.chr 0x02) (to_char (MoveN 0));
+  Alcotest.(check int) "MoveN size" 2 (byte_size (MoveN 0));
+  Alcotest.(check char) "Jz opcode" (Char.chr 0x03) (to_char (Jz 0));
+  Alcotest.(check int) "Jz size" 5 (byte_size (Jz 0));
+  Alcotest.(check char) "Jnz opcode" (Char.chr 0x04) (to_char (Jnz 0));
+  Alcotest.(check int) "Jnz size" 5 (byte_size (Jnz 0));
+  Alcotest.(check char) "In opcode" (Char.chr 0x05) (to_char In);
+  Alcotest.(check int) "In size" 1 (byte_size In);
+  Alcotest.(check char) "Out opcode" (Char.chr 0x06) (to_char Out);
+  Alcotest.(check int) "Out size" 1 (byte_size Out);
+  Alcotest.(check char) "Call opcode" (Char.chr 0x07) (to_char Call);
+  Alcotest.(check int) "Call size" 1 (byte_size Call);
+  Alcotest.(check char) "Clear opcode" (Char.chr 0x08) (to_char Clear);
+  Alcotest.(check int) "Clear size" 1 (byte_size Clear);
+  Alcotest.(check char) "TransferN opcode" (Char.chr 0x09) (to_char (TransferN 0));
+  Alcotest.(check int) "TransferN size" 2 (byte_size (TransferN 0));
+  Alcotest.(check char) "ScanN opcode" (Char.chr 0x0A) (to_char (ScanN 0));
+  Alcotest.(check int) "ScanN size" 2 (byte_size (ScanN 0));
+  Alcotest.(check char) "AddAt opcode" (Char.chr 0x0B) (to_char (AddAt (0, 0)));
+  Alcotest.(check int) "AddAt size" 3 (byte_size (AddAt (0, 0)));
+  Alcotest.(check char) "MulTransfer opcode" (Char.chr 0x0C) (to_char (MulTransfer []));
+  Alcotest.(check int) "MulTransfer size empty" 2 (byte_size (MulTransfer []));
+  Alcotest.(check int) "MulTransfer size 2" 6 (byte_size (MulTransfer [ 1, 2; 3, 4 ]))
+;;
+
+let test_encode_i8_ranges () =
+  (* AddN/MoveN/TransferN/ScanN i8 boundary checks *)
+  expect_ok_bytes "AddN 127" (AddN 127) [ 0x01; 0x7F ];
+  expect_ok_bytes "AddN -128" (AddN (-128)) [ 0x01; 0x80 ];
+  expect_error "AddN 128" (AddN 128);
+  expect_error "AddN -129" (AddN (-129));
+  expect_ok_bytes "MoveN 127" (MoveN 127) [ 0x02; 0x7F ];
+  expect_ok_bytes "MoveN -128" (MoveN (-128)) [ 0x02; 0x80 ];
+  expect_error "MoveN 128" (MoveN 128);
+  expect_error "MoveN -129" (MoveN (-129));
+  expect_ok_bytes "TransferN 5" (TransferN 5) [ 0x09; 0x05 ];
+  expect_error "TransferN 128" (TransferN 128);
+  expect_ok_bytes "ScanN -1" (ScanN (-1)) [ 0x0A; 0xFF ]
+;;
+
+let test_encode_addat_ranges () =
+  expect_ok_bytes "AddAt (0,0)" (AddAt (0, 0)) [ 0x0B; 0x00; 0x00 ];
+  expect_ok_bytes "AddAt (-128,127)" (AddAt (-128, 127)) [ 0x0B; 0x80; 0x7F ];
+  expect_error "AddAt (128,0)" (AddAt (128, 0));
+  expect_error "AddAt (0,128)" (AddAt (0, 128))
+;;
+
+let test_encode_jumps_32bit () =
+  expect_ok_bytes "Jz 0" (Jz 0) (0x03 :: int32_bytes_le 0);
+  expect_ok_bytes "Jz -1" (Jz (-1)) (0x03 :: int32_bytes_le (-1));
+  expect_ok_bytes "Jnz 42" (Jnz 42) (0x04 :: int32_bytes_le 42)
+;;
+
+let test_encode_multransfer () =
+  (* empty pairs *)
+  expect_ok_bytes "MulTransfer []" (MulTransfer []) [ 0x0C; 0x00 ];
+  (* two pairs *)
+  expect_ok_bytes
+    "MulTransfer 2"
+    (MulTransfer [ 1, 2; -3, -4 ])
+    [ 0x0C; 0x02; 0x01; 0x02; 0xFD; 0xFC ];
+  (* count bounds *)
+  let pairs_128 = List.init 128 (fun _ -> 0, 0) in
+  (match encode (MulTransfer pairs_128) with
+   | Ok _ -> Alcotest.fail "MulTransfer with 128 pairs should error"
+   | Error _ -> ());
+  (* operand bounds error mapping *)
+  match encode (MulTransfer [ 200, 0 ]) with
+  | Ok _ -> Alcotest.fail "MulTransfer bad delta should error"
+  | Error _ -> ()
+;;
+
+let test_encode_list_and_combine () =
+  match encode_list [ AddN 1; MoveN (-2); In; Jz 5 ] with
+  | Error _ -> Alcotest.fail "encode_list should succeed"
+  | Ok list_bytes ->
+    let code = combine_encoded_list list_bytes in
+    Alcotest.(check (list int))
+      "combined"
+      [ 0x01; 0x01; 0x02; 0xFE; 0x05; 0x03; 0x05; 0x00; 0x00; 0x00 ]
+      (bytes_to_list code)
+;;
+
+let () =
+  let open Alcotest in
+  run
+    "ISA tests"
+    [ "meta", [ test_case "to_char and byte_size" `Quick test_to_char_and_size ]
+    ; ( "encode"
+      , [ test_case "i8 ranges" `Quick test_encode_i8_ranges
+        ; test_case "addat ranges" `Quick test_encode_addat_ranges
+        ; test_case "jumps 32bit" `Quick test_encode_jumps_32bit
+        ; test_case "multransfer" `Quick test_encode_multransfer
+        ; test_case "encode_list/combine" `Quick test_encode_list_and_combine
+        ] )
+    ]
+;;
