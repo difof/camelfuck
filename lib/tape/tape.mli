ec42989cf6b650a5ab91049f6b67c951c2b1ad50
difof <hesyoamaster@protonmail.com>
2025-08-31 11:31:27 +0330

    refactor: updated Tape interface

--- lib/tape/tape.mli
+++ lib/tape/tape.mli
@@ -24,7 +24,6 @@ val create : ?bias_offset:bias_offset -> ?max_size:int -> int -> t
     Throws `MaxAllocationReached`
 *)
 val move_exn : t -> int -> unit
-val move : t -> int -> (unit, error) result
 
 (* Get the value of current Tape pointer *)
 val get : t -> int
@@ -37,11 +36,12 @@ val add : t -> int -> unit
 
 (* Write at logical offset from current pointer without moving it (masked) *)
 val set_at_offset_exn : t -> int -> int -> unit
-val set_at_offset : t -> int -> int -> (unit, error) result
 
 (* Add a value at logical offset from current pointer without moving it (masked) *)
 val add_at_offset_exn : t -> int -> int -> unit
-val add_at_offset : t -> int -> int -> (unit, error) result
+
+(* Multiply-add transfer from current cell to offsets; zeroes source if non-zero *)
+val multransfer_exn : t -> (int * int) list -> unit
 
 (* Get the total length of the Tape *)
 val len : t -> int
@@ -62,19 +62,19 @@ val max_size : t -> int
    Get a slice of the Tape. 
     If given size is larger than Tape's current position + len, it will throw `BlitOutOfBounds`
 *)
-val blit_out_exn : t -> bytes -> int -> unit
-val blit_out : t -> bytes -> int -> (unit, error) result
+val blit_out_exn : t -> int array -> int -> unit
 
 (*
    Copy the given buffer's contents into Tape, starting at current tape pointer. 
     If Tape limits are reached, it won't grow and throw `BlitOutOfBounds` instead.
 *)
-val blit_in_exn : t -> bytes -> int -> unit
-val blit_in : t -> bytes -> int -> (unit, error) result
+val blit_in_exn : t -> int array -> int -> unit
 
 (*
    Copy the given buffer's contents into Tape, starting at current tape pointer. 
     It will grow the Tape as needed.
 *)
-val blit_in_ensure_exn : t -> bytes -> int -> unit
-val blit_in_ensure : t -> bytes -> int -> (unit, error) result
+val blit_in_ensure_exn : t -> int array -> int -> unit
+
+(* Scan in the direction of [delta] until a zero cell is found; single final pointer move *)
+val scan_to_zero_exn : t -> int -> unit
