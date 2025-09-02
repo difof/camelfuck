484c87078c419e03c96ce30a93f59baf9783ff8e
difof <hesyoamaster@protonmail.com>
2025-09-02 12:28:33 +0330

    test(refactor): merged ClearN instructions

--- test/isa/test_isa.ml
+++ test/isa/test_isa.ml
@@ -55,10 +55,8 @@ let test_to_char_and_size () =
   Alcotest.(check char) "MulTransfer opcode" (Char.chr 0x0C) (to_char (MulTransfer []));
   Alcotest.(check int) "MulTransfer size empty" 2 (byte_size (MulTransfer []));
   Alcotest.(check int) "MulTransfer size 2" 6 (byte_size (MulTransfer [ 1, 2; 3, 4 ]));
-  Alcotest.(check char) "ClearMoveN opcode" (Char.chr 0x0D) (to_char (ClearMoveN 0));
-  Alcotest.(check int) "ClearMoveN size" 2 (byte_size (ClearMoveN 0));
-  Alcotest.(check char) "ClearN opcode" (Char.chr 0x0E) (to_char (ClearN 0));
-  Alcotest.(check int) "ClearN size" 2 (byte_size (ClearN 0))
+  Alcotest.(check char) "ClearN opcode" (Char.chr 0x0E) (to_char (ClearN (0, false)));
+  Alcotest.(check int) "ClearN size" 3 (byte_size (ClearN (0, false)))
 ;;
 
 let test_encode_i8_ranges () =
@@ -74,8 +72,8 @@ let test_encode_i8_ranges () =
   expect_ok_bytes "TransferN 5" (TransferN 5) [ 0x09; 0x05 ];
   expect_error "TransferN 128" (TransferN 128);
   expect_ok_bytes "ScanN -1" (ScanN (-1)) [ 0x0A; 0xFF ];
-  expect_ok_bytes "ClearMoveN -3" (ClearMoveN (-3)) [ 0x0D; 0xFD ];
-  expect_ok_bytes "ClearN 2" (ClearN 2) [ 0x0E; 0x02 ]
+  expect_ok_bytes "ClearN 2, false" (ClearN (2, false)) [ 0x0E; 0x02; 0x00 ];
+  expect_ok_bytes "ClearN -3, true" (ClearN (-3, true)) [ 0x0E; 0xFD; 0x01 ]
 ;;
 
 let test_encode_addat_ranges () =
